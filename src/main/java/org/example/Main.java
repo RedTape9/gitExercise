2b293b390f9d811713fd721c8e279413b9dab0a2
Sergej Jaudszims <redtape9688@gmail.com>
2023-11-16 12:23:13 +0100

    add logout feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("Moin");
         System.out.println("model created");
         System.out.println("Login");
+        System.out.println("Logout");
 
 
 
